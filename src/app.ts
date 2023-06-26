0fab44dc531bdccd4fd2b7e7b26953b54d7a7524
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-26 20:54:36 +0100

    feat: GET request for coffee endpoint and test

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,13 @@ import express from 'express';
 
 export const app = express();
 
+app.use(express.json());
+
+// home
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
+
+// coffeelover
 app.get('/coffeelover', (req, res) => res.send('I like coffee!'));
+
+// coffee
+app.get('/coffee', (req, res) => res.json({ drinkType: 'Coffee', name: 'Latte' }));
