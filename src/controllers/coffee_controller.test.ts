f14a4e564774a497de88c67b3d0469cc78f08181
Hayley Ashby <hayleyashby7@gmail.com>
2023-06-26 21:26:10 +0100

    feat: coffee endpoint GET request handles specific coffeename query parameter

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -11,4 +11,14 @@ describe('Coffee Controller', () => {
 			name: 'Latte',
 		});
 	});
+
+	test('GET /coffee should return correct object when specific coffeename query provided', async () => {
+		const res = await request(app).get('/coffee').query({ coffeeName: 'Pumpkin Spiced Latte' });
+
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toEqual({
+			drinkType: 'Coffee',
+			name: 'Pumpkin Spiced Latte',
+		});
+	});
 });
